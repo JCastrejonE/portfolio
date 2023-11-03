e207b33d43672f7b792b4a8c4aaa5fa2777e52dd
Juan Castrejon <jayz@xbacked.io>
2023-11-03 00:05:50 +0000

    UPD avatar extension

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import { Popover, Transition } from '@headlessui/react'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
-import avatarImage from '@/images/avatar.png'
+import avatarImage from '@/images/avatar.jpg'
 
 function CloseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
